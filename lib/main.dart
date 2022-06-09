1df16651c2b4dd35bd5c62726147903a330417a8
MyChiNehihi <ntmchi146@gmail.com>
2022-06-09 18:15:57 +0700

    profilepage

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-       home: LoginForm(),
+       home: ProfilePage(),
      // initialRoute: HomeScreen.routeName,
      // routes: routes,
     );
