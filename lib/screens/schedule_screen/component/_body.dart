fe6ec92e569e996bea52646e3d3bdbbfb5d41546
ducnm2k <ducnm2k@gmail.com>
2022-07-11 13:12:40 +0700

    update get schedule  tournament, round by list

---
+++ lib/screens/schedule_screen/component/_body.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Container _buildTabView(String e){
+  return Container(
+    child: Center(
+      child: Text(e),
+    ),
+  );
+}
--- lib/screens/schedule_screen/schedule_screen.dart
+++ lib/screens/schedule_screen/schedule_screen.dart
@@ -9,15 +9,56 @@ class ScheduleScreen extends StatefulWidget {
 }
 
 class _ScheduleScreenState extends State<ScheduleScreen> {
-  List<String> tournamentsList = [];
-  List<String> roundsList = [];
+  List<String> tournamentsList = [
+    'tournament 1',
+    'tournament 2',
+    'tournament 3',
+    'tournament 4',
+  ];
+  List<String> roundsList = [
+    'round 1',
+    'round 2',
+    'round 3',
+    'round 4',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
           appBar: buildAppBar(),
-          body: ScheduleBody(),
+          body: DefaultTabController(
+            length: roundsList.length,
+            child: Scaffold(
+                appBar: AppBar(
+                  centerTitle: true,
+                  backgroundColor: Colors.transparent,
+                  title: PreferredSize(
+                      child: TabBar(
+                          isScrollable: true,
+                          unselectedLabelColor: Colors.white.withOpacity(0.7),
+                          indicatorColor: Colors.white,
+                          tabs:
+                          roundsList.map((e) => Tab(child: Text(e),)).toList(),
+                      ),
+                      preferredSize: Size.fromHeight(30.0)),
+                  actions: <Widget>[
+                    PopupMenuButton(
+                        icon: Icon(Icons.calendar_today_sharp),
+                        itemBuilder:(context) =>
+                        tournamentsList.map((e) => PopupMenuItem(child: Text(e),)).toList(),
+                    ),
+                  ],
+                ),
+                body: TabBarView(
+                  children:
+                    roundsList.map((e) => Container(
+                      child: Center(
+                        child: Text(e),
+                      ),
+                    )).toList(),
+                )),
+          ),
         )
     );
   }
