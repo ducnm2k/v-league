4bd76f2d55308aab0a5b57126fc305b29d0a67a6
MyChiNehihi <ntmchi146@gmail.com>
2022-07-16 11:44:29 +0700

    details match and cau thu detail

--- lib/screens/football_match_schedule_and_football_player_screen/cauthu.dart
+++ lib/screens/football_match_schedule_and_football_player_screen/cauthu.dart
@@ -10,6 +10,15 @@ class CauThu extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+
+          Text(
+            "Đội hình",
+            style: TextStyle(
+              color: Colors.grey,
+              fontWeight: FontWeight.normal,
+              fontSize: 20,
+            ),
+          ),
           SizedBox(
             height: 10,
           ),
@@ -37,7 +46,85 @@ class CauThu extends StatelessWidget {
                 ),
               ),
             ],
-          )),
+          ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            "assets/images/quanghai.jpg",
+                            height: 80,
+                            width: 80,
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(
+                              top: 2,
+                              left: 2,
+                              right: 2,
+                            ),
+                            child: Text(
+                              "Quang Hải",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Text(
+            "Dự bị",
+            style: TextStyle(
+              color: Colors.grey,
+              fontWeight: FontWeight.normal,
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            child: Row(
+              children: [
+                Image.asset(
+                  "assets/images/quanghai.jpg",
+                  height: 80,
+                  width: 80,
+                ),
+                Container(
+                  padding: EdgeInsets.only(
+                    top: 2,
+                    left: 2,
+                    right: 2,
+                  ),
+                  child: Text(
+                    "Quang Hải",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.only(top: 10),
             child: Row(
